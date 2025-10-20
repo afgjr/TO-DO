318bf4e8aaad6af8037851e8c2583ac555ddb798
Abel <abelfekede2312@gmail.com>
2025-10-20 19:36:05 +0300

    clear-code-and-add-icons

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 
-
 let tasks=[]
 let  num=0
+
+
 app.get('/',(req,res)=>{
     res.render('index.ejs',{list:tasks})
-   
 })
+
 
 app.post('/add',(req,res)=>{
   const data=req.body.data
@@ -23,6 +24,8 @@ app.post('/add',(req,res)=>{
   }
   res.redirect('/')
 })
+
+
 app.patch('/edit/:ID',(req,res)=>{
   const userID = parseInt(req.params.ID)
   const indexOftask=tasks.findIndex(task=>task.id===userID)
@@ -32,15 +35,16 @@ app.patch('/edit/:ID',(req,res)=>{
   }
   res.status(200).json({message:'successful'})
 })
+
+
 app.delete('/delete/:ID',(req,res)=>{
   const userID = parseInt(req.params.ID)
-  console.log(userID)
   tasks=tasks.filter(task=>task.id!==userID)
-  console.log(tasks)
   res.status(204).send()
-  
 })
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 }); 
+
